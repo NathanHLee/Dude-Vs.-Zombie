86fed37450d2a80d176f2a064da56f68e72e4b93
NathanHLee <nathanhiroyukilee@gmail.com>
2021-12-27 13:22:06 -0800

    Added Base Project

---
+++ Dude Vs Zombies/Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+    private float speed = 10;
+    bool jump = false;
+
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+    */
+
+    
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+    
+}

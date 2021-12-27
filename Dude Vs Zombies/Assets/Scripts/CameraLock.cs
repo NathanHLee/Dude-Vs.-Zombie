a2f8861bbf5ee64a5106339389c8c35bd46b7b1a
NathanHLee <nathanhiroyukilee@gmail.com>
2021-12-27 15:04:16 -0800

    Added bounds for the camera

--- Dude Vs Zombies/Assets/Scripts/CameraLock.cs
+++ Dude Vs Zombies/Assets/Scripts/CameraLock.cs
@@ -8,6 +8,7 @@ public class CameraLock : MonoBehaviour
     public Vector3 offset;
     [Range(1, 10)]
     public int smoothFactor;
+    public Vector3 minValues, maxValues;
 
     private void FixedUpdate()
     {
@@ -17,7 +18,13 @@ public class CameraLock : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+
+        Vector3 boundsPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundsPosition, smoothFactor * Time.fixedDeltaTime);
         transform.position = smoothPosition;
     }
 }
